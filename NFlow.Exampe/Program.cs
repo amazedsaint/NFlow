d3c11a0d25c9d71349ee0b03c9c47a6816207644
Anoop <amazedsaint@gmail.com>
2021-04-03 23:14:05 +0530

    Initial Commit

---
+++ NFlow.Exampe/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using Flow.Access;
+using Grpc.Core;
+using NFlow.Grpc;
+using static Flow.Access.AccessAPI;
+
+namespace NFlow.Exampe
+{
+    class Program
+    {
+
+        static AccessAPIClient client;
+
+
+        static void Main(string[] args)
+        {
+
+
+            var url = "127.0.0.1:3569";
+            var account = "f8d6e0586b0a20c7";
+
+
+            Channel channel = new Channel(url, ChannelCredentials.Insecure);
+            client = new AccessAPIClient(channel);
+            Console.WriteLine("\n--------------------------");
+
+            //Let us do a ping and hopefully with out errors
+            Console.WriteLine("Ping Example");
+            var pingResponse = Ping();
+            Console.WriteLine(pingResponse.ToString());
+            Console.WriteLine("\n--------------------------");
+
+
+            //Shows how to fetch an account
+            Console.WriteLine("Get account response");
+            var accountResponse = GetAccount(account);
+            var accountId = accountResponse.Account.Address.FromByteStringToHex();
+            Console.Write(accountId);
+            Console.WriteLine("\n--------------------------");
+
+
+            //Shows how to execute a script
+            Console.WriteLine("Execute a script");
+            var result=ExecuteScript("pub fun main(): Int { return 1 }");
+            var payload = result.Value.FromByteStringToString();
+            Console.Write(payload);
+            Console.WriteLine("\n--------------------------");
+
+            Console.Read();
+
+        }
+
+
+        public static PingResponse Ping()
+        {
+            var req = new PingRequest();
+            return client.Ping(req);
+        }
+
+
+        public static GetAccountResponse GetAccount(string address)
+        {
+
+            var req = new GetAccountRequest() { Address = address.FromHexToByteString() };
+            return client.GetAccount(req);
+        }
+
+
+        public static ExecuteScriptResponse ExecuteScript(string script, string[] arguments = null)
+        {
+
+            var req = new ExecuteScriptAtLatestBlockRequest() { Script = script.FromStringToByteSring() };
+            return client.ExecuteScriptAtLatestBlock(req);
+        }
+
+    }
+}
---
+++ NFlow.Grpc/HexStringExtensions.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Text;
+
+namespace NFlow.Grpc
+{
+    public static class ProtoStringExtensions
+    {
+        public static byte[] Pack(this String hex)
+        {
+            int NumberChars = hex.Length;
+            byte[] bytes = new byte[NumberChars / 2];
+            for (int i = 0; i < NumberChars; i += 2)
+                bytes[i / 2] = Convert.ToByte(hex.Substring(i, 2), 16);
+            return bytes;
+        }
+
+        public static string Unpack(this byte[] ba)
+        {
+            StringBuilder hex = new StringBuilder(ba.Length * 2);
+            foreach (byte b in ba)
+                hex.AppendFormat("{0:x2}", b);
+            return hex.ToString();
+        }
+
+        public static Google.Protobuf.ByteString FromHexToByteString(this string hex)
+        {
+            var addressByte = hex.Pack();
+            return Google.Protobuf.ByteString.CopyFrom(addressByte);
+        }
+
+        public static string FromByteStringToHex(this Google.Protobuf.ByteString bs)
+        {
+            return bs.ToByteArray().Unpack();
+        }
+
+        public static Google.Protobuf.ByteString FromStringToByteSring(this string str)
+        {
+            return Google.Protobuf.ByteString.CopyFromUtf8(str);
+        }
+
+        public static string FromByteStringToString(this Google.Protobuf.ByteString str)
+        {
+            return str.ToStringUtf8();
+        }
+
+    }
+
+
+
+}
